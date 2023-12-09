d1013e4258c525cdd67268337509e3515899e093
unknown <winternrabbit@gmail.com>
2023-12-09 17:36:06 +0900

    fix(CarNameConfig): 자동차 이름 최소 글자수 값 수정

--- src/main/java/racingcar/config/CarNameConfig.java
+++ src/main/java/racingcar/config/CarNameConfig.java
@@ -2,6 +2,6 @@ package racingcar.config;
 
 public class CarNameConfig {
     public static final String PARSER = ",";
-    public static final int MIN_CAR_NAME_LENGTH = 0;
+    public static final int MIN_CAR_NAME_LENGTH = 1;
     public static final int MAX_CAR_NAME_LENGTH = 5;
 }
