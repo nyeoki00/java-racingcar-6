8f1a06d6cd059463fccc1d483ef4179034d965ea
unknown <winternrabbit@gmail.com>
2023-12-09 17:38:41 +0900

    test(CarNameValidator): 입력 값 parser 후 길이가 0일 경우 예외 처리 되는지 확인

--- src/test/java/racingcar/validator/CarNameValidatorTest.java
+++ src/test/java/racingcar/validator/CarNameValidatorTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.config.CarNameConfig;
@@ -12,8 +13,8 @@ import racingcar.config.CarNameConfig;
 class CarNameValidatorTest {
     CarNameValidator validator = new CarNameValidator();
 
-    @DisplayName("각 자동차 이름이 0~5자 이내가 아닐 경우 예외 발생")
-    @ValueSource(strings = {"abcdef,f,g", ",,f", ",e,"})
+    @DisplayName("각 자동차 이름이 1~5자 이내가 아닐 경우 예외 발생")
+    @ValueSource(strings = {"abcdef,f,g", ",,f", "e, "})
     @ParameterizedTest
     void checkCarNameLength(String inputCarNames) {
         checkIllegalArgumentException(inputCarNames);
@@ -26,8 +27,14 @@ class CarNameValidatorTest {
         checkIllegalArgumentException(inputCarNames);
     }
 
+    @DisplayName("입력 받은 값을 자른 후 리스트의 길이가 0일 경우 예외 발생")
+    @Test
+    void checkCarNamesEmpty() {
+        checkIllegalArgumentException(",,");
+    }
+
     private void checkIllegalArgumentException(String inputCarNames) {
-        List<String> carNames = Arrays.stream(inputCarNames.split(CarNameConfig.PARSER)).toList();
+        List<String> carNames = Arrays.stream(inputCarNames.split(CarNameConfig.PARSER)).map(String::trim).toList();
 
         assertThatThrownBy(() -> validator.validateCarName(carNames)).isInstanceOf(IllegalArgumentException.class);
     }
