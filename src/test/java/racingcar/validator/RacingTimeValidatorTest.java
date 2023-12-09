4bba13309ca4a5e465448612d7c7a18fff642a40
unknown <winternrabbit@gmail.com>
2023-12-09 19:13:53 +0900

    test(RacingTimeValidator): 1~100,000 외의 숫자일 경우 예외 처리 되는지 확인

--- src/test/java/racingcar/validator/RacingTimeValidatorTest.java
+++ src/test/java/racingcar/validator/RacingTimeValidatorTest.java
@@ -12,6 +12,17 @@ class RacingTimeValidatorTest {
     @ValueSource(strings = {"가나다", "aiew", "123wdf", "igl452", "3df9r8gjs35", "", " "})
     @ParameterizedTest
     void checkNotNumber(String racingTime) {
+        checkIllegalArgumentException(racingTime);
+    }
+
+    @DisplayName("1~2147483647 이외의 숫자 입력할 경우 예외 발생하는지 확인")
+    @ValueSource(strings = {"100001", "-3", "0"})
+    @ParameterizedTest
+    void checkOutOfRange(String racingTime) {
+        checkIllegalArgumentException(racingTime);
+    }
+
+    private void checkIllegalArgumentException(String racingTime) {
         RacingTimeValidator RacingTimeValidator = new RacingTimeValidator();
 
         assertThatThrownBy(() -> RacingTimeValidator.isValid(racingTime)).isInstanceOf(IllegalArgumentException.class);
